40477990a71b5484e96b92861b3483bfc67909a2
bhataasim1 <bhataasim700@gmail.com>
2024-12-31 15:31:44 +0530

    Wrap body in QueryWrapper for improved context management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryWrapper from "@/components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <QueryWrapper>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300`}
+        >
+          {children}
+        </body>
+      </QueryWrapper>
     </html>
   );
 }
