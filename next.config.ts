87c7d7ef94af61d49b2520ad793186c678f7796c
bhataasim1 <bhataasim700@gmail.com>
2024-12-31 16:36:25 +0530

    Update image configuration to use remote patterns for avatar URLs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // images: {
+  //   domains: [
+  //     "avatars.githubusercontent.com"
+  //   ],
+  // }
   images: {
-    domains: [
-      "avatars.githubusercontent.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: '',
+        pathname: '/u/*',
+      }
     ],
   }
 };
